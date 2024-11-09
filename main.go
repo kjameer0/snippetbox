43915660af63b5ac3d0e791fd17b21fad8ab61a9
Khalid Jameer <41968688+kjameer0@users.noreply.github.com>
2024-11-08 20:25:44 -0500

    feat: added new test routes

--- main.go
+++ main.go
@@ -9,10 +9,18 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Form for creating a new snippet"))
+}
 func main() {
 	//mux is the part of the app that guides requests
 	//to the url that matches their path
 	http.HandleFunc("/", home)
+	http.HandleFunc("/snippet/view", home)
+	http.HandleFunc("/snippet/create", home)
 	err := http.ListenAndServe(":4000", nil)
 	log.Fatal(err)
 }
